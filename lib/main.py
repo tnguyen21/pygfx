aa59dfb88d3d57c546090a20a2be9dd2b6c885af
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2023-09-01 14:29:30 -0500

    add QOL cli flags (save, show)

--- lib/main.py
+++ lib/main.py
@@ -4,6 +4,8 @@ from shaders import *
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("image", help="Path to the image.")
+    parser.add_argument("--save", default=False, help="Save the image to the current directory.", action=argparse.BooleanOptionalAction)
+    parser.add_argument("--show", default=False, help="Show the image in a window.", action=argparse.BooleanOptionalAction)
     args = parser.parse_args()
 
     img = cv2.imread(args.image)
@@ -12,11 +14,15 @@ if __name__ == "__main__":
         print("Error: Could not open or find the image.")
         exit()
     
-    new_img = to_websafe(img)
+    new_img = ordered_dither(img)
 
-    cv2.imshow('Original', img)
-    # cv2.imshow('Filter', box_blur(img, 5))
-    # cv2.imshow('Filter', to_websafe(img))
-    cv2.imshow('Filter', bw_floyd_steinberg_dither(img))
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    if args.save:
+        out_img_path = args.image.split('.')[0] + '_out.png'
+        print(f"Saving image to {out_img_path}")
+        cv2.imwrite(out_img_path, new_img)
+
+    if args.show:
+        cv2.imshow('Original', img)
+        cv2.imshow('Filter', new_img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
