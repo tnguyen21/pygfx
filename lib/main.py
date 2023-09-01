9aff25e77ab467b9744793ed50708933b2405a8b
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2023-09-01 14:32:55 -0500

    format code

--- lib/main.py
+++ lib/main.py
@@ -4,25 +4,35 @@ from shaders import *
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("image", help="Path to the image.")
-    parser.add_argument("--save", default=False, help="Save the image to the current directory.", action=argparse.BooleanOptionalAction)
-    parser.add_argument("--show", default=False, help="Show the image in a window.", action=argparse.BooleanOptionalAction)
+    parser.add_argument(
+        "--save",
+        default=False,
+        help="Save the image to the current directory.",
+        action=argparse.BooleanOptionalAction,
+    )
+    parser.add_argument(
+        "--show",
+        default=False,
+        help="Show the image in a window.",
+        action=argparse.BooleanOptionalAction,
+    )
     args = parser.parse_args()
 
     img = cv2.imread(args.image)
-    
+
     if img is None:
         print("Error: Could not open or find the image.")
         exit()
-    
+
     new_img = ordered_dither(img)
 
     if args.save:
-        out_img_path = args.image.split('.')[0] + '_out.png'
+        out_img_path = args.image.split(".")[0] + "_out.png"
         print(f"Saving image to {out_img_path}")
         cv2.imwrite(out_img_path, new_img)
 
     if args.show:
-        cv2.imshow('Original', img)
-        cv2.imshow('Filter', new_img)
+        cv2.imshow("Original", img)
+        cv2.imshow("Filter", new_img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
--- lib/shaders.py
+++ lib/shaders.py
@@ -1,17 +1,20 @@
 import numpy as np
 import cv2
 
+
 def box_blur(img: np.ndarray, kernel_sz: int = 3) -> np.ndarray:
     kernel = np.ones((kernel_sz, kernel_sz), np.float32) / kernel_sz**2
     return cv2.filter2D(img, -1, kernel)
+
 
 def to_websafe(img: np.ndarray) -> np.ndarray:
     levels = [0, 51, 102, 153, 204, 255]
     lut = np.zeros(256, np.uint8)
     for i in range(256):
         lut[i] = min(levels, key=lambda x: abs(x - i))
-    
+
     return cv2.LUT(img, lut)
+
 
 def ordered_dither(img: np.ndarray) -> np.ndarray:
     M = np.array([[0, 2], [3, 1]]) / 4
@@ -22,14 +25,15 @@ def ordered_dither(img: np.ndarray) -> np.ndarray:
             old_pixel = img[y, x]
             new_pixel = [min(255, p + M[y % 2, x % 2] * 255) for p in old_pixel]
             new_img[y, x] = new_pixel
-    return new_img    
+    return new_img
+
 
 def tommy_dither(img: np.ndarray) -> np.ndarray:
     """
     This isn't really dithering. I'm doing a bunch of messed up math.
     But it creates a cool effect, so I'm keeping it
     """
-    
+
     img = img.astype(np.float32)
     levels = [0, 51, 102, 153, 204, 255]
     lut = np.zeros(256, np.uint8)
@@ -45,11 +49,11 @@ def tommy_dither(img: np.ndarray) -> np.ndarray:
             new_img[y, x] = new_pixel
             error = old_pixel - new_pixel
             if x < w - 1:
-                img[y, x+1] += error * 7 / 16
+                img[y, x + 1] += error * 7 / 16
             if y < h - 1:
                 if x > 0:
-                    img[y+1, x-1] += error * 3 / 16
-                img[y+1, x] += error * 5 / 16
+                    img[y + 1, x - 1] += error * 3 / 16
+                img[y + 1, x] += error * 5 / 16
                 if x < w - 1:
-                    img[y+1, x+1] += error * 1 / 16
+                    img[y + 1, x + 1] += error * 1 / 16
     return new_img
