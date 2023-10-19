c5315cd6dfa9a28cfaf2e9e2d31df2f838717c79
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2023-10-18 20:39:13 -0500

    add kuwahara filter

--- lib/shaders.py
+++ lib/shaders.py
@@ -69,3 +69,38 @@ def tommy_dither(img: np.ndarray) -> np.ndarray:
                 if x < w - 1:
                     img[y + 1, x + 1] += error * 1 / 16
     return new_img
+
+def kuwahara(img: np.ndarray, ksize: int = 3) -> np.ndarray:
+    """
+    Apply Kuwahara filter to an image.
+    
+    :param src: Input image
+    :param ksize: Size of the kernel. Must be odd and greater than 1.
+    :return: Filtered image
+    """
+    h, w = img.shape[:2]
+    new_img = np.zeros((h, w, 3), dtype=img.dtype)
+
+    pad = np.ceil(ksize / 2).astype(int)
+    img_pad = cv2.copyMakeBorder(img, *[pad]*4, cv2.BORDER_DEFAULT)
+
+    for y in range(h):
+        for x in range(w):
+            regions = [
+                ((y, x), (y+ksize, x+ksize)), # top left
+                ((y, x+ksize-1), (y+ksize, x+2*ksize-1)), # top right
+                ((y+ksize-1, x), (y+2*ksize-1, x+ksize)), # bottom left
+                ((y+ksize-1, x+ksize-1), (y+2*ksize-1, x+2*ksize-1)) # bottom right
+            ]
+
+            min_var = float('inf')
+            for r1, r2 in regions:
+                y1, x1 = r1
+                y2, x2 = r2
+                mean, var = cv2.meanStdDev(img_pad[y1:y2, x1:x2])
+                avg_var = var.mean()
+                if avg_var < min_var:
+                    min_var = avg_var
+                    new_img[y, x] = mean.astype(np.uint8).reshape(3,)
+    
+    return new_img
